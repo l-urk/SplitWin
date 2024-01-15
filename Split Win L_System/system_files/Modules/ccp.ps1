a04ea8f796b763959dd1c38dec546366086459ed
LKIRA <112792604+l-urk@users.noreply.github.com>
2024-01-14 19:39:01 -0800

    Update ccp.ps1

--- Split Win L_System/system_files/Modules/ccp.ps1
+++ Split Win L_System/system_files/Modules/ccp.ps1
@@ -1,17 +1,13 @@
-$processesToCheck = @("powershell","powershell_ise", "python", "python2","python3", "cmd", "conhost")
+$processesToCheck = @("powershell","powershell_ise", "python", "python2", "python3", "cmd", "conhost", "openconsole", "terminal", "kali", "wsl", "notepad", "explorer", "notepad++", "notepad.exe")
 $targetProcessCount = 21
-
 while ($true) {
     $combinedCount = 0
-
     foreach ($processName in $processesToCheck) {
         $runningProcesses = Get-Process -Name $processName -ErrorAction SilentlyContinue
-
         if ($runningProcesses) {
             $combinedCount += $runningProcesses.Count
         }
     }
-
     if ($combinedCount -ge $targetProcessCount) {
         Write-Host "Combined count of processes reached $combinedCount. Terminating processes..."
         foreach ($processName in $processesToCheck) {
@@ -19,10 +15,10 @@ while ($true) {
             if ($runningProcesses) {
                 foreach ($process in $runningProcesses) {
                     Stop-Process -Id $process.Id -Force
+					
                 }
             }
         }
     }
-
     Start-Sleep -Seconds 1  # Check process count every 1 second
 }
